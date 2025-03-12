ebf5ef4efa2f81901f9743c44ea0604159ed140c
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-12 19:01:25 +0530

    add files

--- Javascript/S5E1.js
+++ Javascript/S5E1.js
@@ -1,1 +1,9 @@
-console.log("hello");
+// Functions in JavaScript*(iportant topic in java script)
+// We always use parentheses symbol "()" or bracakets
+// Fuction defination
+function myfunction(params) {
+    console.log("Welcome");
+    console.log("We are learing JS");
+}
+// Call a function 
+myfunction();
