8dab7fb85529c416d96a72d6c8bb0f01cf8eec17
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-12 19:37:23 +0530

    add files

--- Javascript/S5E1.js
+++ Javascript/S5E1.js
@@ -8,3 +8,19 @@ function myfunction() {
 // Call a function 
 myfunction();//Function can be call many as needed
 
+function jifunction(msg){//parameter->input (msg) is the paramerter in this
+    console.log(msg);
+}
+jifunction("learing JS");//the value pass is known as argument here "learing JS" 
+//Here "learing JS" is the argument 
+
+function jifunctions(ms,n){//using new variable for a another argument
+    console.log(ms+n);
+}
+jifunctions("learing JS", 100);//multiple argument can be pass
+
+//Function -> 2 number sum
+function sum(a,b) {//parameter work as local variable
+    console.log(a+b);//variable are defined only for the block
+}
+sum(5,2);
