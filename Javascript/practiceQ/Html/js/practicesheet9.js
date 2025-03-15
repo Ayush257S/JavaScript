8f0cd0cace204cdbc4e8094bdddf25a8a16a6217
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-15 23:51:28 +0530

    add file

--- Javascript/practiceQ/Html/js/practicesheet9.js
+++ Javascript/practiceQ/Html/js/practicesheet9.js
@@ -1,6 +1,10 @@
 //Practice Q1
-let num=[1,2,3,4,5,6,7,8,9,10];
+let num=[87, 93, 45, 67, 89, 34, 56, 99, 78, 90];
 let pass=num.filter((val)=>{
-    return val>5;
-})
+    return val>80;
+});
 console.log(pass);
+
+//Practice Q2
+prompt("Enter the number : ");
+let arr=[];
