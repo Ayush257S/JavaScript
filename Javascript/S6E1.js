fe7074746486ac25a43920eb4ef95c99f88c906d
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-16 19:15:30 +0530

    add files

---
+++ Javascript/S6E1.js
@@ -0,0 +1,7 @@
+//DOM document object model
+// dom is object oriented representation of web page
+// dom is interface that allows scripts to dynamically access and update content, structure and style of a document
+// dom represents the document as nodes and objects
+
+console.dir(document);//shows the document object
+console.log(document);//shows the html document
