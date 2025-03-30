f38670c206efb8bb433c848f0dfc22f51f16f433
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-30 23:08:22 +0530

    add fiels

--- Javascript/S8E1.js
+++ Javascript/S8E1.js
@@ -4,7 +4,16 @@
 const students={
     fullname:"Ayush",
     marks:95.7,
-    printmarks: function(){
+    printmarks: function(){ //can also be written as printmarks(){
         console.log("marks =",this.marks);//here this means student.marks
     },
 };
+//JavaScript objects have a special property called prototype(prototype itself is an object)
+
+
+const empolyee={
+    calcTax(){
+        console.log("Tax rate is 10%");
+    }
+};
+empolyee.calcTax();
