ff536f0ead673978234f22bce2688426c8662c14
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-05 22:04:44 +0530

    add files

--- Javascript/S8E2.js
+++ Javascript/S8E2.js
@@ -1,1 +1,31 @@
 //Inheritance in js
+// Example one
+class Parent{
+    hello(){
+        console.log("hello")
+    }
+}
+
+class Child extends Parent{}
+
+let obj = new Child();
+
+
+//Example two 
+class Person{
+    constructor(){
+        console.log("Enter parent constructor");
+        this.species="homo sapiens";
+    }
+    eat(){
+        console.log("Eat");
+    }
+
+    sleep(){
+        console.log("Sleep");
+    }
+
+    work(){
+        console.log("Do nothing");
+    }
+}
