2797e433c4ba4ec51b02701717cc725f23d45d44
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-05 22:07:03 +0530

    add files

--- Javascript/S8E2.js
+++ Javascript/S8E2.js
@@ -30,4 +30,20 @@ class Person{
     }
 };
 
+class engineer extends Person{//here the engineer(child class) inhert the properties of Person(parent)
+    constructor(branch){
+        console.log("Enter child constructor");
+        super();//It is used to invoke parent class constructor
+        this.branch= branch;
+        console.log("Exit child constructor");
+    }
+    work(){//if child and parent have same functions the child will get's more priority
+        console.log("solve problems");//this is also know as method overridding
+    }
+}
+
+let personobj= new engineer();
+let p1= new Person();
+
+
 
