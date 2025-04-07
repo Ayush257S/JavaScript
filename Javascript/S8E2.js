c90962c806e798174c084de9f4880aeee47164b8
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-07 15:17:53 +0530

    add files

--- Javascript/S8E2.js
+++ Javascript/S8E2.js
@@ -13,9 +13,10 @@ let obj = new Child();
 
 //Example two 
 class Person{
-    constructor(){
+    constructor(name){
         console.log("Enter parent constructor");
         this.species="homo sapiens";
+        this.name=name;
     }
     eat(){
         console.log("Eat");
@@ -31,13 +32,15 @@ class Person{
 };
 
 class engineer extends Person{//here the engineer(child class) inhert the properties of Person(parent)
-    constructor(branch){
+    constructor(branch, name){
         console.log("Enter child constructor");
-        super();//It is used to invoke parent class constructor
+        //Constructor can be in parent class by passing same method in super
+        super(name);//It is used to invoke parent class constructor
         this.branch= branch;
         console.log("Exit child constructor");
     }
-    work(){//if child and parent have same functions the child will get's more priority
+    work(){//if child and parent have same functions the child will get's more priority 
+        super.eat();//Is used to directly accecing the parents method directly
         console.log("solve problems");//this is also know as method overridding
     }
 };
@@ -49,5 +52,8 @@ let p1= new Person();
 //Is used to call a constructor of its parent class.
 //Access the parent class properties and methods.
 
-let engobj = new engineer("Computer Science");//super is used in a constructor
-
+let engobj = new engineer("Computer Science", "JavaScript");//super is used in a constructor
+console.log(engobj.name);//Accessing the properties of child class
+console.log(engobj.species);//Accessing the properties of parent class
+console.log(engobj.branch);//Accessing the properties of child class
+console.log(engobj.eat());//Accessing the method of parent class
