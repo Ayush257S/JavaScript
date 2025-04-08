7bf81f4789bf297601777a3a1882fa22dde56c23
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-08 21:01:00 +0530

    add files

--- Javascript/practiceQ/Html/js/practicesheet13.js
+++ Javascript/practiceQ/Html/js/practicesheet13.js
@@ -1,5 +1,15 @@
+// Pratice Q1
 class user {
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+
     view() {
         console.log("view user");
     }
+
 }
+ 
+let student1 = new user("ABC","abc123@gmial.com");
+let student2 = new user("BCD","bcd234@gmail.com");
