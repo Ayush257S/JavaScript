a6b83c4a29b42f3730640e9e14e0e52e2e541db7
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-09 21:51:53 +0530

    add files

--- Javascript/S9E1.js
+++ Javascript/S9E1.js
@@ -11,5 +11,18 @@ setTimeout(()=>{
     console.log("hello");
 },5000); // Asynchronous code, will run after 5 seconds
 
-console.log("four");
-console.log("five");
+console.log("four");//This will run before the setTimeout function is executed
+console.log("five");//It will not wait for the setTimeout function to finish before executing this line of code
+
+//Callbacks
+function sum(a,b){
+    console.log(a+b);
+}
+function calculator(a,b, sumCallback){
+    sumCallback(a+b);
+}
+
+calculator(2,5, (a,b)=>{
+    console.log(a+b);
+});
+
