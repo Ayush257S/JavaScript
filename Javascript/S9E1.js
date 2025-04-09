12849d42370550ec2f56477b3adc4eb203cc2c37
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-09 21:42:22 +0530

    add files

--- Javascript/S9E1.js
+++ Javascript/S9E1.js
@@ -1,1 +1,12 @@
-//Async await 
+//Synchronous
+//Synchronous code is executed in the order it is written
+console.log("one");
+console.log("two");
+console.log("three");
+
+//Asynchronous
+//Asynchronous code is executed in the background, allowing other code to run while waiting for a task to complete
+
+setTimeout(()=>{
+    console.log("hello");
+},5000); // Asynchronous code, will run after 5 seconds
