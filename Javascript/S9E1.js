cdf2b3a5cfbf5e09b744f00d99d7b66f9765da0c
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-11 11:33:04 +0530

    add files

--- Javascript/S9E1.js
+++ Javascript/S9E1.js
@@ -9,7 +9,7 @@ console.log("three");
 
 setTimeout(()=>{
     console.log("hello");
-},5000); // Asynchronous code, will run after 5 seconds
+},10200); // Asynchronous code, will run after 5 seconds
 
 console.log("four");//This will run before the setTimeout function is executed
 console.log("five");//It will not wait for the setTimeout function to finish before executing this line of code
@@ -29,5 +29,33 @@ calculator(2,5, (a,b)=>{
 
 //Callback hell
 //Callback hell is a situation where multiple nested callbacks make the code difficult to read and maintain
-//Nesting 
+//Nesting means putting one function inside another function or loops etc.
+
+function getData(DataID,getNextData){
+    setTimeout(()=>{
+        console.log("Data",DataID);
+        if(getNextData){
+            getNextData();
+        }
+    },2000);
+}
+
+//This is a callback hell example
+//It is difficult to read and maintain
+//Nested callbacks stackd up and make pyramid structure
+//This forms priamid of doom or callback hell
+getData(1,()=>{
+    console.log("getting data 2...");
+    getData(2,()=>{
+        console.log("getting data 3...");
+        getData(3,()=>{
+            console.log("getting data 4...");
+            getData(4,()=>{
+                console.log("getting data 5...");
+                getData(5);
+            });
+        });
+    });
+});
+
 
