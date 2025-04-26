4c49a36608f2af33188fda79f6f5bf17b9a24b5a
Ayush257S <ayushsrivastva2103@gmail.com>
2025-04-26 13:24:56 +0530

    add files

---
+++ Javascript/S9E3.js
@@ -0,0 +1,27 @@
+// Promise Chaining or promise chain
+
+function getData(DataID){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Data",DataID);
+            resolve("Success");
+        },2000);
+    });
+};
+
+
+// getData(1).then((res)=>{
+//     console.log(res);
+//     getData(2).then((res)=>{
+//         console.log(res);
+//     })
+// });
+
+//More better way to write
+getData(1).then((res)=>{//first getData 1 is resolved
+    return getData(2);//then data 2 is resolved
+}).then((res)=>{
+    return getData(3);
+}).then((res)=>{
+    console.log(res);
+});
